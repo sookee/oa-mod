d8fd1c3f1e3e09285207ccc4b18ae37d83ad4ac9
FennelFetish <fennel@alchemists.ch>
2013-05-18 01:09:25 +0200

    changed format of the log messages

--- oa-0.8.8/code/game/katina.c
+++ oa-0.8.8/code/game/katina.c
@@ -27,37 +27,37 @@ void katina_write(int clientNum, katina_t* stats)
     for(i=0; i<WP_NUM_WEAPONS; ++i)
     {
         // Weapon Usage Update
-        // KATINA: 0 <client#> <weapon#> <#shotsFired>
+        // WeaponUsage: <client#> <weapon#> <#shotsFired>
         if(stats->numShots[i])
-            G_LogPrintf( "KATINA: %i %i %i %i\n", KLT_WEAPON_USAGE, clientNum, i, stats->numShots[i]);
+            G_LogPrintf( "WeaponUsage: %i %i %i\n", clientNum, i, stats->numShots[i]);
     }
     
     for(i=0; i<MOD_NUM_DAMAGETYPES; ++i)
     {
         // MOD (Means of Death) Damage Update
-        // KATINA: 1 <client#> <mod#> <#hits> <damageDone> <#hitsRecv> <damageRecv>
+        // MODDamage: <client#> <mod#> <#hits> <damageDone> <#hitsRecv> <damageRecv>
         if(stats->numHits[i] || stats->numHitsRecv[i] || stats->damageDone[i] || stats->damageRecv[i])
         {
-            G_LogPrintf( "KATINA: %i %i %i %i %i %i %i\n",
-                KLT_MOD_DAMAGE, clientNum, i,
+            G_LogPrintf( "MODDamage: %i %i %i %i %i %i\n",
+                clientNum, i,
                 stats->numHits[i], stats->damageDone[i],
                 stats->numHitsRecv[i], stats->damageRecv[i]);
         }
     }
     
     // Player Stats Update
-    // KATINA: 2 <client#>
-    //         <fragsFace> <fragsBack> <fraggedInFace> <fraggedInBack>
-    //         <spawnKillsDone> <spanwKillsRecv>
-    //         <pushesDone> <pushesRecv>
-    //         <healthPickedUp> <armorPickedUp>
+    // PlayerStats: <client#>
+    //              <fragsFace> <fragsBack> <fraggedInFace> <fraggedInBack>
+    //              <spawnKillsDone> <spanwKillsRecv>
+    //              <pushesDone> <pushesRecv>
+    //              <healthPickedUp> <armorPickedUp>
     if(stats->fragsFace || stats->fragsBack || stats->fraggedInFace || stats->fraggedInBack
        || stats->spawnKillsDone || stats->spawnKillsRecv
        || stats->pushesDone || stats->pushesRecv
        || stats->healthPickedUp || stats->armorPickedUp)
     {
-        G_LogPrintf( "KATINA: %i %i %i %i %i %i %i %i %i %i %i %i\n",
-            KLT_PLAYER_STATS, clientNum,
+        G_LogPrintf( "PlayerStats: %i %i %i %i %i %i %i %i %i %i %i\n",
+            clientNum,
             stats->fragsFace, stats->fragsBack, stats->fraggedInFace, stats->fraggedInBack,
             stats->spawnKillsDone, stats->spawnKillsRecv,
             stats->pushesDone, stats->pushesRecv,
