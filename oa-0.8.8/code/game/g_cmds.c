5aec3f6a753a7f17b588f1d18841f9fc3f17b517
tobias <w@ingix.de>
2014-05-01 17:09:04 +0200

    who added a return before of my code D:' ...  :D

--- oa-0.8.8/code/game/g_cmds.c
+++ oa-0.8.8/code/game/g_cmds.c
@@ -926,9 +926,29 @@ void Cmd_Team_f( gentity_t *ent ) {
 	
 
 	if( !force ) {
-	    if ( ent->client->switchTeamTime > level.time ) {
-		    trap_SendServerCommand( ent-g_entities, "print \"May not switch teams more than once per 5 seconds.\n\"" );
-		    return;
+
+		trap_Argv( 1, s, sizeof( s ) );
+		//only allow real team switches and do not increase wait-time for missclicking on the same team
+		if ( ((oldTeam==TEAM_BLUE) && Q_strequal( s , "blue" )) || 
+			((oldTeam==TEAM_RED) && Q_strequal( s , "red" )) || 
+			((oldTeam==TEAM_SPECTATOR) && Q_strequal( s , "spectator"))) {
+				switch ( oldTeam ) {
+				case TEAM_BLUE:
+					trap_SendServerCommand( ent-g_entities, "print \"You are already in the Blue Team.\n\"" );
+					break;
+				case TEAM_RED:
+					trap_SendServerCommand( ent-g_entities, "print \"You are already in the Red team.\n\"" );
+					break;
+				default:
+					trap_SendServerCommand( ent-g_entities, "print \"You are already in Spectator Mode.\n\"" );
+				}
+		} else {
+			if ( ent->client->switchTeamTime > level.time ) {
+				trap_SendServerCommand( ent-g_entities, "print \"May not switch teams more than once per 5 seconds.\n\"" );
+			} else{
+				SetTeam( ent, s );
+				ent->client->switchTeamTime = level.time + 5000;
+			}
 		}
 	}
 
@@ -937,30 +957,6 @@ void Cmd_Team_f( gentity_t *ent ) {
 		&& ent->client->sess.sessionTeam == TEAM_FREE ) {
 		ent->client->sess.losses++;
 	}
-
-
-	trap_Argv( 1, s, sizeof( s ) );
-	//only allow real team switches and do not increase wait-time for missclicking on the same team
-	if ( ((oldTeam==TEAM_BLUE) && Q_strequal( s , "blue" )) || 
-		((oldTeam==TEAM_RED) && Q_strequal( s , "red" )) || 
-		((oldTeam==TEAM_SPECTATOR) && Q_strequal( s , "spectator"))) {
-			switch ( oldTeam ) {
-			case TEAM_BLUE:
-				trap_SendServerCommand( ent-g_entities, "print \"You are already in the Blue Team.\n\"" );
-				break;
-			case TEAM_RED:
-				trap_SendServerCommand( ent-g_entities, "print \"You are already in the Red team.\n\"" );
-				break;
-			default:
-				trap_SendServerCommand( ent-g_entities, "print \"You are already in Spectator Mode.\n\"" );
-		}
-
-	} else {
-		SetTeam( ent, s );
-		ent->client->switchTeamTime = level.time + 5000;
-		
-	}
-
 }
 
 
