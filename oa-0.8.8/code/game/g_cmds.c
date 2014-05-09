db182a43ce437fb984d44fa0e9e4952dce7e443b
SooKee <oasookee@gmail.com>
2014-05-09 02:11:04 +0100

    Added log output when player calls a vote

--- oa-0.8.8/code/game/g_cmds.c
+++ oa-0.8.8/code/game/g_cmds.c
@@ -1854,6 +1854,8 @@ void Cmd_CallVote_f( gentity_t *ent ) {
 
         ent->client->pers.voteCount++;
 	trap_SendServerCommand( -1, va("print \"%s called a vote.\n\"", ent->client->pers.netname ) );
+	G_LogPrintf("Callvote: %i %s: %s has called a vote for %s\n", ent-g_entities, arg1
+			, ent->client->pers.netname, arg1);
 
 	// start the voting, the caller autoamtically votes yes
 	level.voteTime = level.time;
