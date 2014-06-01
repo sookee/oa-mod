5cbd869fedf41bef84ca2f056a35345131084e92
SooKee <oasookee@gmail.com>
2014-06-01 11:33:07 +0100

    stuff

--- scripts/merge-all.sh
+++ scripts/merge-all.sh
@@ -5,7 +5,7 @@ git checkout -b sookee-0.1
 git pull
 git checkout -b sookee-0.2
 git pull
-git merge sookee-0.1
+git merge --commit sookee-0.1
 git push
 
 # wing-0.2
@@ -13,7 +13,7 @@ git checkout -b wing
 git pull
 git checkout -b wing-0.2
 git pull
-git merge wing
+git merge --commit wing
 git push
 
 # fennel-0,2
@@ -21,22 +21,22 @@ git checkout -b fennel-0.1
 git pull
 git checkout -b fennel-0.2
 git pull
-git merge fennel-0.1
+git merge --commit fennel-0.1
 git push
 
 #katina-0.1
 git checkout -b katina-0.1
 git pull
-git merge fennel-0.1
-git merge sookee-0.1
-git merge wing
+git merge --commit fennel-0.1
+git merge --commit sookee-0.1
+git merge --commit wing
 git push
 
 #katina-0.2
 git checkout -b katina-0.2
-git merge katina-0.1
-git merge fennel-0.2
-git merge sookee-0.2
-git merge wing-0.2
+git merge --commit katina-0.1
+git merge --commit fennel-0.2
+git merge --commit sookee-0.2
+git merge --commit wing-0.2
 git push
 
