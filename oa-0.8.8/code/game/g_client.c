6c6c7dfc1fab526e07839ee9382a96316828e103
SooKee <oasookee@gmail.com>
2014-06-14 04:54:42 +0100

    repositionned session.save_path flag setting

--- oa-0.8.8/code/game/g_client.c
+++ oa-0.8.8/code/game/g_client.c
@@ -635,7 +635,8 @@ void ClientRespawn( gentity_t *ent ) {
 					LMSpoint();	
                                 //Sago: This is really bad
                                 //TODO: Try not to make people spectators here
-				ent->client->sess.spectatorState = PM_SPECTATOR;
+				// sookee: fox wrong enum type
+				ent->client->sess.spectatorState = SPECTATOR_FOLLOW;
                                 //We have to force spawn imidiantly to prevent lag.
                                 ClientSpawn(ent);
 			}
@@ -1583,14 +1584,6 @@ char *ClientConnect( int clientNum, qboolean firstTime, qboolean isBot ) {
 	}
 
 
-	// SooKee add IP address to log
-	// There is a bug in the engine that ocasionally the wrong guid & ip
-	// are present. They can not be trusted until the ClientUserinfoChanged
-	// event that occurs after this function exits.
-	// Hence here I set a flag that the next ClientUserInfoChanged is trustworthy
-	if(firstTime && !isBot)
-		client_userinfo_ready[clientNum] = qtrue;
-
 	// if a player reconnects quickly after a disconnect, the client disconnect may never be called, thus flag can get lost in the ether
 	if (ent->inuse) {
 		G_LogPrintf("Forcing disconnect on active client: %i\n", clientNum);
@@ -1634,6 +1627,14 @@ char *ClientConnect( int clientNum, qboolean firstTime, qboolean isBot ) {
 	//KK-OAX Swapped these in order...seemed to help the connection process.
 	// get and distribute relevent paramters
 	ClientUserinfoChanged( clientNum );
+
+	// SooKee add IP address to log
+	// There is a bug in the engine that ocasionally the wrong guid & ip
+	// are present. They can not be trusted until the ClientUserinfoChanged
+	// event that occurs after this function exits.
+	// Hence here I set a flag that the next ClientUserInfoChanged is trustworthy
+	if(firstTime && !isBot)
+		client_userinfo_ready[clientNum] = qtrue;
 
 	G_LogPrintf( "ClientConnect: %i\n", clientNum );
 
@@ -2268,9 +2269,6 @@ void ClientDisconnect( int clientNum ) {
 	int			i;
     char	    userinfo[MAX_INFO_STRING];
 
-    // sookee: tracking reliable ClientConnectInfo
-    client_userinfo_ready[clientNum] = qfalse;
-
 	// cleanup if we are kicking a bot that
 	// hasn't spawned yet
 	G_RemoveQueuedBotBegin( clientNum );
@@ -2343,6 +2341,9 @@ void ClientDisconnect( int clientNum ) {
 
         if ( ent->client->pers.connected == CON_CONNECTED && ent->client->sess.sessionTeam != TEAM_SPECTATOR)
             PlayerStore_store(Info_ValueForKey(userinfo,"cl_guid"),ent->client->ps);
+
+        // sookee: tracking reliable ClientConnectInfo
+        client_userinfo_ready[clientNum] = qfalse;
 
 	G_LogPrintf( "ClientDisconnect: %i\n", clientNum );
 
