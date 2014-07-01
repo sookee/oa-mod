9be149e53026bbcfccf87592739f9a2586a7acb6
SooKee <oasookee@gmail.com>
2014-07-01 01:19:53 +0100

    Add: "client:" message to log containing slot number of player about to talk Ver: Bump to 0.2.2

--- oa-0.8.8/code/game/g_cmds.c
+++ oa-0.8.8/code/game/g_cmds.c
@@ -1167,11 +1167,13 @@ void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *chatText ) 
 	switch ( mode ) {
 	default:
 	case SAY_ALL:
+		G_LogPrintf( "client: %d:\n", ent - g_entities ); // sookee client number of person about to act
 		G_LogPrintf( "say: %s: %s\n", ent->client->pers.netname, chatText );
 		Com_sprintf (name, sizeof(name), "%s%c%c"EC": ", ent->client->pers.netname, Q_COLOR_ESCAPE, COLOR_WHITE );
 		color = COLOR_GREEN;
 		break;
 	case SAY_TEAM:
+		G_LogPrintf( "client: %d:\n", ent - g_entities ); // sookee client number of person about to act
 		G_LogPrintf( "sayteam: %s: %s\n", ent->client->pers.netname, chatText );
 		if (Team_GetLocationMsg(ent, location, sizeof(location)))
 			Com_sprintf (name, sizeof(name), EC"(%s%c%c"EC") (%s)"EC": ", 
