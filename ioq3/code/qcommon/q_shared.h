494bf4201dbe372649579b13705148e1008d3969
SooKee <oasookee@gmail.com>
2015-02-27 01:15:54 +0000

    changed info string from 1024 to 2048 in ioq3

--- ioq3/code/qcommon/q_shared.h
+++ ioq3/code/qcommon/q_shared.h
@@ -231,7 +231,7 @@ typedef int		clipHandle_t;
 #define	MAX_STRING_TOKENS	1024	// max tokens resulting from Cmd_TokenizeString
 #define	MAX_TOKEN_CHARS		1024	// max length of an individual token
 
-#define	MAX_INFO_STRING		1024
+#define	MAX_INFO_STRING		2048
 #define	MAX_INFO_KEY		  1024
 #define	MAX_INFO_VALUE		1024
 
